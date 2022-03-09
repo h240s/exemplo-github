0d61beaf4191b37dd51b131f32e60db5f4c9302c
Gitgoro <gitgoro05@email.com>
2022-03-08 21:59:18 -0400

    estilizaçao quase finalizada

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-import './style.css'
+const $ = document.querySelector.bind(document)
 
+const addTask = (event) => {
+  event.preventDefault()
+  console.log('Entrou')
+  const taskDescription = $('#task-description').value
 
+  const newTaskItem = `<li>${taskDescription}</li>`
+  
+  
+  $('#task-list').insertAdjacentHTML('beforeend', newTaskItem)
+  $('#task-form').reset()
+}
